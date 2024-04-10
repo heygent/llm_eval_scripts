c5c548be20f4463b79d7a492718d733931665140
heygent <emanuele.gentiletti@gmail.com>
2024-04-10 19:01:48 +0200

    Initial commit

---
+++ model_configurations.py
@@ -0,0 +1,36 @@
+from bitsandbytes import BitsAndBytesConfig
+from lm_eval.models.huggingface import HFLM
+from lm_eval.models.vllm_causallms import VLLM
+
+
+def _default_quantization_config():
+    return BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype="torch.float16",
+    )
+
+
+def mixtral(**kwargs):
+    return HFLM(
+        pretrained="mistralai/Mixtral-8x7B-Instruct-v0.2",
+        quantization_config=_default_quantization_config(),
+        device_map="auto",
+        **kwargs
+    )
+
+
+def mixtral_flashattention(**kwargs):
+    return mixtral(
+        attn_implementation="flash_attention_2",
+        **kwargs
+    )
+
+
+def mixtral_vllm(**kwargs):
+    return VLLM(
+        pretrained="mistralai/Mixtral-8x7B-Instruct-v0.2",
+        quantization_config=_default_quantization_config(),
+        device_map="auto",
+        **kwargs
+    )
