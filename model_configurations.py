6eaf21a4d9cf1fda0c8df04067bbe7fdf0af1cc1
heygent <emanuele.gentiletti@gmail.com>
2024-04-11 07:45:29 +0200

    Bug fixing and wandb logging

--- model_configurations.py
+++ model_configurations.py
@@ -1,4 +1,5 @@
-from bitsandbytes import BitsAndBytesConfig
+import torch
+from transformers import BitsAndBytesConfig
 from lm_eval.models.huggingface import HFLM
 from lm_eval.models.vllm_causallms import VLLM
 
@@ -7,13 +8,13 @@ def _default_quantization_config():
     return BitsAndBytesConfig(
         load_in_4bit=True,
         bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype="torch.float16",
+        bnb_4bit_compute_dtype=torch.float16,
     )
 
 
 def mixtral(**kwargs):
     return HFLM(
-        pretrained="mistralai/Mixtral-8x7B-Instruct-v0.2",
+        pretrained="mistralai/Mixtral-8x7B-Instruct-v0.1",
         quantization_config=_default_quantization_config(),
         device_map="auto",
         **kwargs
@@ -29,7 +30,7 @@ def mixtral_flashattention(**kwargs):
 
 def mixtral_vllm(**kwargs):
     return VLLM(
-        pretrained="mistralai/Mixtral-8x7B-Instruct-v0.2",
+        pretrained="mistralai/Mixtral-8x7B-Instruct-v0.1",
         quantization_config=_default_quantization_config(),
         device_map="auto",
         **kwargs
