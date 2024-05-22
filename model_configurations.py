b0642dbf1aaeb56910cdce5c08b0e314b04b8596
heygent <emanuele.gentiletti@gmail.com>
2024-05-22 11:31:53 +0200

    Cli with click

--- model_configurations.py
+++ model_configurations.py
@@ -1,56 +1,38 @@
+from dataclasses import dataclass
 import torch
 from transformers import BitsAndBytesConfig
 from lm_eval.models.huggingface import HFLM
 from lm_eval.models.vllm_causallms import VLLM
 
 
-def bnb_4bit_config():
-    return BitsAndBytesConfig(
+MODEL_SHORTNAMES = {
+    "phi3": "microsoft/Phi-3-mini-4k-instruct",
+    "mixtral": "mistralai/Mixtral-8x7B-Instruct-v0.1" 
+}
+
+
+def makemodel(
+    pretrained: str,
+    flashattention: bool = False,
+    vllm: bool = False,
+    quantization_4bit: bool = True,
+    **kwargs
+):
+    if pretrained in MODEL_SHORTNAMES:
+        pretrained = MODEL_SHORTNAMES[pretrained]
+    
+    cls = VLLM if vllm else HFLM
+
+    quantization_config = BitsAndBytesConfig(
         load_in_4bit=True,
         bnb_4bit_quant_type="nf4",
         bnb_4bit_compute_dtype=torch.float16,
-    )
+    ) if quantization_4bit else None
 
-
-def mixtral(**kwargs):
-    return HFLM(
-        pretrained="mistralai/Mixtral-8x7B-Instruct-v0.1",
-        quantization_config=bnb_4bit_config(),
+    return cls(
+        pretrained,
+        attn_implementation='flash_attention_2' if flashattention else None,
+        quantization_config=quantization_config,
         device_map="auto",
         **kwargs
     )
-
-
-def mixtral_flashattention(**kwargs):
-    return mixtral(
-        attn_implementation="flash_attention_2",
-        **kwargs
-    )
-
-
-def mixtral_vllm(**kwargs):
-    return VLLM(
-        pretrained="mistralai/Mixtral-8x7B-Instruct-v0.1",
-        quantization_config=bnb_4bit_config(),
-        device_map="auto",
-        **kwargs
-    )
-
-
-def phi3(**kwargs):
-    return HFLM(
-        pretrained="microsoft/Phi-3-mini-4k-instruct",
-        quantization_config=bnb_4bit_config(),
-        device_map="auto",
-        **kwargs
-    )
-
-
-def phi3_vllm(**kwargs):
-    return VLLM(
-        pretrained="microsoft/Phi-3-mini-4k-instruct",
-        quantization_config=bnb_4bit_config(),
-        device_map="auto",
-        **kwargs
-    )
-
